fc734b1de6f3c38d14d377c2b94b48e14196a709
Maik Riechert <letmaik@outlook.com>
2025-09-14 23:06:14 +0100

    fix path

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  // Use relative paths in production builds so the app can be hosted from any subfolder.
+  base: './',
   plugins: [react()],
 });
